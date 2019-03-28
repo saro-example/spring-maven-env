23156f1f966bd4f612088e5c486353493eecae45
PARK Yong Seo <j@saro.me>
2019-03-28 14:52:18 +0900

    init

---
+++ src/main/java/web/SpringMavenEnvApplication.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenEnvApplication.class, args);
+	}
+
+}
